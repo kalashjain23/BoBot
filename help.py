c7c9518ba6b3e6003ed781893a22409f29c7b64d
kalashjain23 <kalashjain513@gmail.com>
2022-07-09 16:10:44 +0530

    added help message

---
+++ help.py
@@ -0,0 +1,8 @@
+help_msg = '''```
+General syntax for the commands:
+        bo {command} {description (if required)}
+Commands available: 
+- weather {description = city} [gives you the weather in that city]
+- help 
+        ```
+'''
