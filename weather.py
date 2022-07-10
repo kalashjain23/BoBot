3e656f2dc629c2cb05f0524d4dae89f030e279c3
kalashjain23 <kalashjain513@gmail.com>
2022-07-10 15:12:23 +0530

    added temperature function

--- weather.py
+++ weather.py
@@ -1,18 +1,32 @@
 import requests
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+api_id = os.getenv("API_ID")
 
 
 def weather(city):
-    API_ID = os.getenv("API_ID")
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_ID}"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_id}"
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
         description = data['weather'][0]['description']
         return description
+
+    else:
+        return "ERROR!!"
+
+
+def temp(city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_id}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        temperature = data['main']['temp']
+        return round(temperature - 273.15, 3)
 
     else:
         return "ERROR!!"
