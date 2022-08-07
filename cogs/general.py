ddc285ff3d33c35a546a3f8ce59814aa9a951d4c
kalashjain23 <kalashjain513@gmail.com>
2022-08-07 15:53:13 +0530

    changed the bot's presence

--- cogs/general.py
+++ cogs/general.py
@@ -1,13 +1,10 @@
+import discord
 from discord.ext import commands
 
 
 class General(commands.Cog):
     def __int__(self, bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(f"BoBot is online.")
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -6,12 +6,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix='.bo ')
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='.bo ', intents=intents)
 
 
 @bot.command()
 async def load(extension):
     bot.load_extension(f'cogs.{extension}')
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game('with you 😳'))
+    print(f"BoBot is online.")
+
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
