b96a24cac53c21c1246c44019c11d158a6946d85
kalashjain23 <kalashjain513@gmail.com>
2022-08-07 19:36:06 +0530

    added purge command

--- cogs/general.py
+++ cogs/general.py
@@ -6,6 +6,16 @@ class General(commands.Cog):
     def __int__(self, bot):
         self.bot = bot
 
+    @commands.command(name='purge')
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount=10):
+        await ctx.channel.purge(limit=amount)
+
+    @clear.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.channel.send('''```cs\nYou don't have permission to do so!!```''')
+
 
 def setup(bot):
     bot.add_cog(General(bot))
