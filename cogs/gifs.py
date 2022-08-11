b080be0b53898bd11b205ea5bc3d50f7341b62ea
kalashjain23 <kalashjain513@gmail.com>
2022-08-12 00:59:05 +0530

    improvement in gifs output

--- cogs/gifs.py
+++ cogs/gifs.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from APIs import kawai_api
 
@@ -8,24 +9,46 @@ class Gifs(commands.Cog):
         self.url = None
 
     @commands.command(name='hug')
-    async def hug(self, message):
-        self.url = kawai_api.get_response('hug')
-        await message.channel.send(f'{self.url}')
+    async def hug(self, ctx, args=None):
+        if ctx.author.name == ctx.message.mentions[0].name:
+            await ctx.channel.send(f"Don't worry, you're not alone :pleading_face:")
+        elif args is not None:
+            self.url = kawai_api.get_response('hug')
+            embed = discord.Embed(color=0x00FFFF)
+            embed.set_image(url=self.url)
+            await ctx.channel.send(f'**{ctx.author.name}** hugs **{ctx.message.mentions[0].name}** :flushed:', embed=embed)
+        else:
+            embed = discord.Embed(color=0x00FFFF)
+            embed.set_author(name=f'OK but hug who?? :<<')
+            await ctx.channel.send(embed=embed)
 
     @commands.command(name='blush')
-    async def blush(self, message):
+    async def blush(self, ctx):
         self.url = kawai_api.get_response('blush')
-        await message.channel.send(f'{self.url}')
+        embed = discord.Embed(color=0x00FFFF)
+        embed.set_image(url=self.url)
+        await ctx.channel.send(f"**{ctx.author.name}'s** blushing :flushed:", embed=embed)
 
     @commands.command(name='cuddle')
-    async def cuddle(self, message):
-        self.url = kawai_api.get_response('cuddle')
-        await message.channel.send(f'{self.url}')
+    async def cuddle(self, ctx, args=None):
+        if ctx.author.name == ctx.message.mentions[0].name:
+            await ctx.channel.send(f"Don't worry, you're not alone :pleading_face:")
+        elif args is not None:
+            self.url = kawai_api.get_response('cuddle')
+            embed = discord.Embed(color=0x00FFFF)
+            embed.set_image(url=self.url)
+            await ctx.channel.send(f'**{ctx.author.name}** cuddles **{ctx.message.mentions[0].name}** :pleading_face:', embed=embed)
+        else:
+            embed = discord.Embed(color=0x00FFFF)
+            embed.set_author(name=f'OK but cuddle who?? :<<')
+            await ctx.channel.send(embed=embed)
 
     @commands.command(name='dance')
-    async def dance(self, message):
+    async def dance(self, ctx):
         self.url = kawai_api.get_response('dance')
-        await message.channel.send(f'{self.url}')
+        embed = discord.Embed(color=0x00FFFF)
+        embed.set_image(url=self.url)
+        await ctx.channel.send(f"Ayyoo, **{ctx.author.name}** got da moves! :flushed:", embed=embed)
 
 
 def setup(bot):
